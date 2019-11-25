cc3c2da6ff3cbb70881f6d6dda91e381c3829520
Tania Akter <45971852+aktertania@users.noreply.github.com>
2019-11-25 21:18:54 +0600

    Create Student.java

---
+++ Lab1/Student.java
@@ -0,0 +1,13 @@
+package Lab1.WithOnlyObject;
+
+public class Student {
+	public int id;
+    public String name; 
+    public double cgpa; 
+
+    public Student(int id, String name, double cgpa) {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+}
