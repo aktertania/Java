f9a186b315fe240692cbc609bd6e45f67c482338
Tania Akter <45971852+aktertania@users.noreply.github.com>
2019-11-27 13:16:27 +0600

    Create Employee.java

---
+++ Lab2/Aggregation/Employee.java
@@ -0,0 +1,51 @@
+package Lab2.Aggregation;
+
+public class Employee {
+	
+	private long empId;
+    private String empName;
+    private Address address; // aggregation
+    private double salary;
+
+    public long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(long empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", address=" + address +
+                ", salary=" + salary +
+                '}';
+    }
+}
