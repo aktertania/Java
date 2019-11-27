2deb81bcdd83f55dc5fe4202923ef37ea362fd40
Tania Akter <45971852+aktertania@users.noreply.github.com>
2019-11-27 13:12:32 +0600

    Create Address.java

---
+++ Lab2/Aggregation/Address.java
@@ -0,0 +1,51 @@
+package Lab2.Aggregation;
+
+public class Address {
+	
+	private int id;
+    private String line;
+    private String city;
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", line='" + line + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
