e030536ed420270c55ca313a84507fd20ae29a32
Tania Akter <45971852+aktertania@users.noreply.github.com>
2019-12-03 21:18:58 +0600

    Create AbstractionTest.java

---
+++ Lab3/Abstraction/UsingAbstractClass/AbstractionTest.java
@@ -0,0 +1,12 @@
+package Lab3.Abstraction.UsingAbstractClass;
+
+public class AbstractionTest {
+
+	
+		public static void main(String[] args) {
+	        Bank bank = new Brac();
+	        System.out.println(bank.getInterest());
+
+	}
+
+}
